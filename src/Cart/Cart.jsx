d28d5c0645b2663bf83ba545e1782b1119b3ebb3
Shubham Thakur <shubhamdthakur55@gmail.com>
2024-01-31 11:27:04 +0530

    image added

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -1,45 +1,46 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 import { Link } from "react-router-dom";
-import logo from '../assets/nodata.png'
-import '../styling/cart.css'
+import logo from "../assets/cart.png";
+import "../styling/cart.css";
 
 function Cart() {
+  // getting props from redux:
+  const cartItems = useSelector((state) => state.cart.items);
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+  const subTotal = useSelector((state) => state.cart.totalAmount);
+  const selectedAddress = useSelector((state) => state.address.selectedAddress);
+  const { city, street_name, street_address, zip_code, state, country } =
+    selectedAddress;
 
-    // getting props from redux:
-    const cartItems = useSelector((state) => state.cart.items);
-    const cartQuantity = useSelector((state) => state.cart.totalQuantity);
-    const subTotal = useSelector((state) => state.cart.totalAmount);
-    const selectedAddress = useSelector((state) => state.address.selectedAddress)
-    const { city, street_name, street_address, zip_code, state, country } = selectedAddress;
-
-    if (cartItems.length == 0) {
-        return (
-            <div className='nodata-image'><img src={logo} alt="" /></div>
-        )
-    }
-
+  if (cartItems.length == 0) {
     return (
-        <div className="container">
+      <div className="nodata-image">
+        <img src={logo} alt="" />
+      </div>
+    );
+  }
 
-            <div className="Card">
-                <ul>
-                    {
-                        cartItems.map((item) => {
-                            return <CartItem key={item.id} {...item} />
-                        })
-                    }
-                </ul>
-                <div className="cart-summary">
-                    <h2>{`Items : ${cartQuantity}`}</h2>
-                    <h2>{`CartSubtotal : $${subTotal.toFixed(1)} `}</h2>
-                    <br />
-                    <p>{`Address : ${city}, ${street_name}, ${street_address}, ${zip_code}, ${state}, ${country} `}</p>
-                    <Link to={'/cart/thanks'}><button>Proceed to pay</button></Link>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="Card">
+        <ul>
+          {cartItems.map((item) => {
+            return <CartItem key={item.id} {...item} />;
+          })}
+        </ul>
+        <div className="cart-summary">
+          <h2>{`Items : ${cartQuantity}`}</h2>
+          <h2>{`CartSubtotal : $${subTotal.toFixed(1)} `}</h2>
+          <br />
+          <p>{`Address : ${city}, ${street_name}, ${street_address}, ${zip_code}, ${state}, ${country} `}</p>
+          <Link to={"/cart/thanks"}>
+            <button>Proceed to pay</button>
+          </Link>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
