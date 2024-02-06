e2c61f50d9d308ed8b35c8f6dd368f30ff9bd170
Shubham Thakur <shubhamdthakur55@gmail.com>
2024-02-06 16:34:08 +0530

    making resonsive...

--- src/Address/AddressList.jsx
+++ src/Address/AddressList.jsx
@@ -1,30 +1,35 @@
-import React from 'react'
-import { useDispatch } from 'react-redux';
-import { deleteAddress, displayAddress } from '../features/AddressSlice';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteAddress, displayAddress } from "../features/AddressSlice";
 
 function AddressList(props) {
-    const { id, city, street_name, street_address, zip_code, state, country } = props;
-    const dispatch = useDispatch();
+  const { id, city, street_name, street_address, zip_code, state, country } =
+    props;
+  const dispatch = useDispatch();
 
+  const selectAddress = () => {
+    //dispatch address id
+    dispatch(displayAddress({ ...props }));
+  };
 
-    const selectAddress = () => {
-        //dispatch address id
-        dispatch(displayAddress({ ...props }))
-    }
+  const handleDelete = () => {
+    dispatch(deleteAddress(id));
+  };
 
-    const handleDelete = () => {
-        dispatch(deleteAddress(id))
-    }
-
-    return (
-        <li key={id} className='address-list'>
-            <div>
-                <button className='select-button' onClick={selectAddress}> Select </button>
-                {`Address : ${city}, ${street_name}, ${street_address}, ${zip_code}, ${state}, ${country} `}
-                <button className='delete-button' onClick={handleDelete}><i class="gg-trash"></i></button>
-            </div>
-        </li>
-    )
+  return (
+    <li key={id} className="address-list">
+      <div>
+        <button className="select-button" onClick={selectAddress}>
+          {" "}
+          Select{" "}
+        </button>
+        <p>{`Address : ${city}, ${street_name}, ${street_address}, ${zip_code}, ${state}, ${country} `}</p>
+        <button className="delete-button" onClick={handleDelete}>
+          <i class="gg-trash"></i>
+        </button>
+      </div>
+    </li>
+  );
 }
 
-export default AddressList
+export default AddressList;
